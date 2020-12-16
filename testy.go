c879a23ca2e1c2f51a28a7042855edab89592c3c
JD Allen <39197125+john2exonets@users.noreply.github.com>
2020-12-16 13:13:42 -0600

    Update so it works as a Package

--- testy.go
+++ testy.go
@@ -6,21 +6,21 @@ import (
 	"testing"
 )
 
-// notErr() - Check to see if the returned err is NOT nil
+// NotErr - Check to see if the returned err is NOT nil
 func NotErr(t *testing.T, err error) {
 	if err != nil {
 		t.Error(err)
 	}
 }
 
-// isErr()  -  Should return an error condition
+// IsErr  -  Should return an error condition
 func IsErr(t *testing.T, err error, msg string) {
 	if err == nil {
 		t.Error(msg)
 	}
 }
 
-// assert() if the two values are equal, if not error out
+// Assert if the two values are equal, if not error out
 func Assert(t *testing.T, a interface{}, b interface{}) {
 	if a == b {
 		return
@@ -28,7 +28,7 @@ func Assert(t *testing.T, a interface{}, b interface{}) {
 	t.Errorf("Received \"%v\" (type %v), expected \"%v\" (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
 }
 
-// assertNot() that the two values are NOT equal
+// AssertNot - that the two values are NOT equal
 func AssertNot(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
 		return
@@ -37,19 +37,38 @@ func AssertNot(t *testing.T, a interface{}, b interface{}) {
 }
 
 // contains() - does the string contain somewhere the value?
-func Contains(t *testing.T, src string, v string) {
-	if strings.Contains(src, v) {
-		return
-	}
-	t.Errorf("String \"%s\" does not Contain \"%s\"", src, v)
-}
+// func contains(t *testing.T, src string, v string) {
+// 	if strings.Contains(src, v) {
+// 		return
+// 	}
+// 	t.Errorf("String \"%s\" does not Contain \"%s\"", src, v)
+// }
 
-// arrContains() - does the []string contain the value?
-func ArrayContains(t *testing.T, src []string, v string) {
-	for i := range src {
-		if src[i] == v {
+// // arrContains() - does the []string contain the value?
+// func arrContains(t *testing.T, src []string, v string) {
+// 	for i := range src {
+// 		if src[i] == v {
+// 			return
+// 		}
+// 	}
+// 	t.Errorf("Slice \"%v\" does not Contain \"%s\"", src, v)
+// }
+
+// Contains - does the list contain the target?
+// Based on: https://stackoverflow.com/questions/10485743/contains-method-for-a-slice
+func Contains(t *testing.T, target interface{}, list interface{}) {
+	if reflect.TypeOf(list).Kind() == reflect.Slice || reflect.TypeOf(list).Kind() == reflect.Array {
+		listvalue := reflect.ValueOf(list)
+		for i := 0; i < listvalue.Len(); i++ {
+			if target == listvalue.Index(i).Interface() {
+				return
+			}
+		}
+	}
+	if reflect.TypeOf(target).Kind() == reflect.String && reflect.TypeOf(list).Kind() == reflect.String {
+		if strings.Contains(list.(string), target.(string)) {
 			return
 		}
 	}
-	t.Errorf("Slice \"%v\" does not Contain \"%s\"", src, v)
+	t.Errorf("List \"%v\" (type %v) does NOT contain \"%v\" (type %v)", list, reflect.TypeOf(list), target, reflect.TypeOf(target))
 }
